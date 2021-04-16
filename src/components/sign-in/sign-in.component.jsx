406dbe8762c34745a9626b13fe3b2e54c58f0d8e
Maria Cassino <maria.cassino@mckesson.com>
2021-04-16 16:53:55 -0400

    set 'button' as the 'type' for Google sign in button

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -64,7 +64,7 @@ class SignIn extends React.Component {
                     />
                     <div className='buttons'>
                         <CustomButton type='submit'>Sign In</CustomButton>
-                        <CustomButton onClick={signInWithGoogle} isGoogleSignin>
+                        <CustomButton type="button" onClick={signInWithGoogle} isGoogleSignin>
                             {''}
                             Sign in with Google{''}
                         </CustomButton>
