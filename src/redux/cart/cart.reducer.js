4d5bf2f8dd82c2b0cf1e5122b0d4ac31b09f7ef1
Maria Cassino <maria.cassino@mckesson.com@oxc02z60yalvdt.lan>
2021-07-30 14:09:29 -0400

    added addItem and removeItem functionality to checkout item component

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,5 +1,5 @@
 import CartActionTypes from './cart.types';
-import { addItemToCart } from './cart.utils'
+import { addItemToCart, removeItemFromCart } from './cart.utils'
 
 const INITIAL_STATE = {
     hidden: true,
@@ -8,8 +8,8 @@ const INITIAL_STATE = {
 
 const cartReducer = (state = INITIAL_STATE, action) => {
     switch (action.type) {
-        // 'TOGGLE_CART_HIDDEN' is a string but we want it to be an action type, 
-        // so we set the string to a constant in cart.types.js & called it on CartActionTypes below:
+        /* 'TOGGLE_CART_HIDDEN' is a string but we want it to be an action type, 
+        so we set the string to a constant in cart.types.js & called it on CartActionTypes below: */
         case CartActionTypes.TOGGLE_CART_HIDDEN:
             return {
                 ...state,
@@ -20,6 +20,23 @@ const cartReducer = (state = INITIAL_STATE, action) => {
                 ...state,
                 // pass in cartItems from state, and item we want to add
                 cartItems: addItemToCart(state.cartItems, action.payload )
+            }
+        case CartActionTypes.REMOVE_ITEM:
+            return {
+                ...state,
+                // pass in cartItems from state, and item we want to remove
+                cartItems: removeItemFromCart(state.cartItems, action.payload)
+            }
+        case CartActionTypes.CLEAR_ITEM_FROM_CART:
+            return {
+                ...state,
+                /* update cartItems to return a new array without any instance 
+                of the item we're trying to clear away. If cartItem id does not match action.payload id
+                (the item we're trying to remove), keep it. If it does match, filter it out. Filter
+                returns us back anything that yields true as a new array. */
+                cartItems: state.cartItems.filter(
+                    cartItem => cartItem.id !== action.payload.id  
+                )
             }
         default:
             return state;
--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,10 +1,15 @@
-// this const will need to be imported into cart.reducer
+/* 
+this const will need to be imported into cart.reducer
+*/
 export const addItemToCart = (cartItems, cartItemToAdd) => {
     const existingCartItem = cartItems.find(
         cartItem => cartItem.id == cartItemToAdd.id 
     );
 
-    // map through cartItems, take each cartItem, if cartItem exists, basically just add 1 to it, or return the cartItem
+    /* 
+    map through cartItems, take each cartItem, if cartItem exists, 
+    basically just add 1 to it, or return the cartItem
+    */
     if (existingCartItem) {
         return cartItems.map(cartItem => 
             cartItem.id == cartItemToAdd.id
@@ -13,7 +18,43 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
         );
     }
 
-    // if cartItem is not found in array, return new array w/ existing cartItems, but 
-    // also add in `{ ...cartItemToAdd, quantity: 1}` object
+    /* 
+    if cartItem is not found in array, return new array w/ existing cartItems, but 
+    also add in `{ ...cartItemToAdd, quantity: 1}` object 
+    */
     return [...cartItems, { ...cartItemToAdd, quantity: 1}];
-}
+};
+
+
+/* 
+this const will need to be imported into cart.reducer
+*/
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+
+    const existingCartItem = cartItems.find(
+        cartItem => cartItem.id == cartItemToRemove.id 
+    );
+
+    /* 
+    if there are cartItems, filter out the ones that match the id of cartItemToRemove and 
+    keep the rest.
+    */
+    if (existingCartItem.quantity == 1) {
+        /*
+        `filter` keeps the items where the function returns `true`. If the id is not the one
+        we want to remove, we keep the item. If it is, we want to remove the item entirely.
+        */
+        return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id);
+    }
+
+    /*
+    if existingCartItem is greater than one and it matches cartItemToRemove's id, 
+    we don't remove the entire item, we just decrease it by one
+    */
+    return cartItems.map(
+        cartItem => 
+        cartItem.id == cartItemToRemove.id ?
+            {...cartItem, quantity: cartItem.quantity - 1}
+            : cartItem
+    )
+};
