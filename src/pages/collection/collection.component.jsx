96f8b6a3b2299ae1029cc03197d12489f636b219
Maria Cassino <maria.cassino@mckesson.com@oxc02z60yalvdt.lan>
2021-08-20 15:57:10 -0400

    changed Category naming to Collection

---
+++ src/pages/collection/collection.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+/* component that displays our items */
+import CollectionItem from '../../components/collection-item/collection-item.component';
+
+import './collection.styles.scss';
+
+const CollectionPage = ({match}) => (
+    <div className='collection-page'>
+        <h2>COLLECTION PAGE</h2>
+    </div>
+);
+
+export default CollectionPage;
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 
-import CollectionsOverview from '../../components/collections-overview/collections-overview.component'
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
+import CollectionPage from '../collection/collection.component';
 
-const ShopPage = ({collections}) => (
+/* We have access to the object `match` here bc in App.js, our /shop page
+is nested in a route, and <Route/> automatically passes the 3 objects
+`match`, `location`, and `history` via the props  */
+/* We need to tell our route that the route name is a param (`/shop/:category`, with 
+category being hats, jackets, etc.) */
+const ShopPage = ( {match} ) => (
   <div className='shop-page'>
-    <CollectionsOverview />
+    <Route exact path={`${match.path}`} component={CollectionsOverview} />
+    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
   </div>
 );
 
