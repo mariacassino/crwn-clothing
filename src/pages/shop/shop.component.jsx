5140af8595ff3e270b341ed44b5846bb8c8b89fa
Maria Cassino <maria.cassino@mckesson.com>
2021-12-28 16:59:00 -0500

    finished spinner component; refactored componentDidMount in shop component to use a .get()

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -4,10 +4,16 @@ import { connect } from 'react-redux';
 
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 import CollectionPage from '../collection/collection.component';
-
 import { firestore, convertCollectionSnapshotToMap } from '../../firebase/firebase.utils';
 
 import { updateCollections } from '../../redux/shop/shop.actions';
+
+import WithSpinner from '../../components/with-spinner/with-spinner.component';
+
+const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview);
+const CollectionPageWithSpinner = WithSpinner(CollectionPage);
+
+
 
 /* We have access to the object `match` here bc in App.js, our /shop page
 is nested in a route, and <Route/> automatically passes the 3 objects
@@ -15,6 +21,10 @@ is nested in a route, and <Route/> automatically passes the 3 objects
 /* We need to tell our route that the route name is a param (`/shop/:category`, with 
 category being hats, jackets, etc.) */
 class ShopPage extends React.Component { 
+  state = {
+    loading: true
+  };
+
   /* unsubscribe from snapshot representation of collections array that we get from Firestore after 
   fetching it from componentDidMount() */
   unsubscribeFromSnapshot = null;
@@ -28,18 +38,20 @@ class ShopPage extends React.Component {
     this collectionRef will send us the snapshot respresenting the code of our `collections` object's
     array at the time this code renders. We'll want to perform some async requests bc the data is 
     on the actual objects inside the snapshot.  */
-    collectionRef.onSnapshot(async snapshot => {
+    collectionRef.get().then(snapshot => {
       const collectionsMap = convertCollectionSnapshotToMap(snapshot);
       updateCollections(collectionsMap);
+      this.setState({loading: false});
     });
   }
 
   render() {
     const { match } = this.props;
+    const { loading } = this.state;
     return (
       <div className='shop-page'>
-        <Route exact path={`${match.path}`} component={CollectionsOverview} />
-        <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+        <Route exact path={`${match.path}`} render={(props) => <CollectionsOverviewWithSpinner isLoading={loading} {...props} />} />
+        <Route path={`${match.path}/:collectionId`} render={(props) => <CollectionPageWithSpinner isLoading={loading} {...props} />} />
       </div>
     );
   }
