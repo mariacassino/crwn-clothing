6dae5f0a9b1089e51aed8b461e0eecaa4d59237c
Maria Cassino <maria.cassino@mckesson.com>
2021-12-29 16:57:53 -0500

    changed == to === in cart.utils.js & store.js

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -3,7 +3,7 @@ this const will need to be imported into cart.reducer
 */
 export const addItemToCart = (cartItems, cartItemToAdd) => {
     const existingCartItem = cartItems.find(
-        cartItem => cartItem.id == cartItemToAdd.id 
+        cartItem => cartItem.id === cartItemToAdd.id 
     );
 
     /* 
@@ -12,7 +12,7 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
     */
     if (existingCartItem) {
         return cartItems.map(cartItem => 
-            cartItem.id == cartItemToAdd.id
+            cartItem.id === cartItemToAdd.id
             ? { ...cartItem, quantity: cartItem.quantity + 1 }
             : cartItem
         );
@@ -32,14 +32,14 @@ this const will need to be imported into cart.reducer
 export const removeItemFromCart = (cartItems, cartItemToRemove) => {
 
     const existingCartItem = cartItems.find(
-        cartItem => cartItem.id == cartItemToRemove.id 
+        cartItem => cartItem.id === cartItemToRemove.id 
     );
 
     /* 
     if there are cartItems, filter out the ones that match the id of cartItemToRemove and 
     keep the rest.
     */
-    if (existingCartItem.quantity == 1) {
+    if (existingCartItem.quantity === 1) {
         /*
         `filter` keeps the items where the function returns `true`. If the id is not the one
         we want to remove, we keep the item. If it is, we want to remove the item entirely.
@@ -53,7 +53,7 @@ export const removeItemFromCart = (cartItems, cartItemToRemove) => {
     */
     return cartItems.map(
         cartItem => 
-        cartItem.id == cartItemToRemove.id ?
+        cartItem.id === cartItemToRemove.id ?
             {...cartItem, quantity: cartItem.quantity - 1}
             : cartItem
     )
